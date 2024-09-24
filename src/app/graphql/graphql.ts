2c1514671d6e812b5eac2c65a75e78cc89af5651
monakamon <>
2024-09-25 02:00:54 +0900

    init

---
+++ src/app/graphql/graphql.ts
@@ -0,0 +1,10 @@
+import { initGraphQLTada } from 'gql.tada';
+import type { introspection } from './graphql-env.d.ts';
+
+export const graphql = initGraphQLTada<{
+  introspection: introspection;
+  scalars: {
+    DateTime: string;
+    JSON: any;
+  };
+}>();
